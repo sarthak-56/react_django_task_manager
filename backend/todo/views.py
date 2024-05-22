b42265fcf97ebc58091a42a39fd999687c6e09e1
Sarthak Ranjan Mishra <125554009+sarthak-56@users.noreply.github.com>
2024-05-22 11:53:57 +0530

    Update views.py

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 
-
-# viewsets class provides the implementation for CRUD operations by default, what we had to do was specify the serializer class and the query set.
-
-from rest_framework import viewsets          # add this
-from .serializers import TodoSerializer      # add this
-from .models import Todo                     # add this
+from rest_framework import viewsets     
+from .serializers import TodoSerializer
+from .models import Todo       
 
 
-class TodoView(viewsets.ModelViewSet):       # add this
-    serializer_class = TodoSerializer          # add this
-    queryset = Todo.objects.all()              # add this
+class TodoView(viewsets.ModelViewSet):  
+    serializer_class = TodoSerializer        
+    queryset = Todo.objects.all()          
